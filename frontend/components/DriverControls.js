5ee48c8f420ed1119b023894ffe1fb91373f1f0e
izuharo <p.marwolo5@gmail.com>
2025-11-05 01:03:33 -0500

    creation of drivercontrols and driverpanel components for eventual driver portal page. driver route select acts as an intermediate page for drivers to select the shuttle they're operating.

---
+++ frontend/components/DriverControls.js
@@ -0,0 +1,2 @@
+/*Works in addition with DriverPanel, on click for any of the elements, DriverStatus will be updated to reflect
+the change in route/status/etc.*/
---
+++ frontend/pages/driver_route_select.js
@@ -0,0 +1,30 @@
+/*Transition page between sign in screen and 
+driver portal. After being authenticated, the
+driver will select one of three routes (more
+can be implemented as needed), which will then transfer
+them to the appropiate websites. For the demo, a driver
+dummy acount will need to be made to access the driver
+portals.*/
+
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export default function DriverRouteSelect() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Navbar></Navbar>
+            <main 
+            className="container mx-auto bg-white items-center flex flex-col flex-grow pt-40">
+
+                <div id="selectRoute" 
+                className="items-center bg-white shadow-md rounded-md flex flex-col gap-3 p-6">
+                    <h1>Select Your Shuttle</h1>
+                    <a id="doermerButton" className="bg-[#cfb991] text-black font-semibold px-6 py-3 rounded-md hover:bg-yellow-400">Doermer</a>
+                    <a id="canterburyButton" className="bg-[#cfb991] text-black font-semibold px-6 py-3 rounded-md hover:bg-yellow-400">Canterbury Green</a>
+                    <a id="housingButton" className="bg-[#cfb991] text-black font-semibold px-6 py-3 rounded-md hover:bg-yellow-400">Housing</a>
+                </div>
+            </main>
+            <Footer></Footer>
+        </div>
+    );
+}
