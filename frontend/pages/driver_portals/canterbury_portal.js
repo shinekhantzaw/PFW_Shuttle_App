fb9040266e28b64769e6d78ae79050efaf1af65c
izuharo <p.marwolo5@gmail.com>
2025-11-07 20:16:18 -0500

    quick bugfix.

--- frontend/pages/driver_portals/canterbury_portal.js
+++ frontend/pages/driver_portals/canterbury_portal.js
@@ -11,7 +11,6 @@ export default function CanterburyPortal() {
         <div className="flex flex-col min-h-screen">
             <main 
             className="w-full mx-auto bg-[url('/canterbury.png')] bg-cover bg-center items-center flex flex-col flex-grow pt-40">
-                <div className="bg-white rounded-md flex flex-col gap-3 p-6"></div>
                 <DriverDashboard shuttle={shuttle} controlOptions={controlOptions}></DriverDashboard>
             </main>
         </div>
